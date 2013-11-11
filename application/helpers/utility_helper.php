6b1914f67a5aa3925343b0a5aec4a63ef2225123
Vishal Agrawal <vishalagrawal14@gmail.com>
2013-11-11 14:00:15 -0500

    add utility helper

---
+++ application/helpers/utility_helper.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ *	function to return the url of the assests folder
+ */
+function asset_url()
+{
+   return base_url().'assets/';
+}
+
+/* End of file utility_helper.php */
+/* Location: ./application/helpers/utility_helper.php */
