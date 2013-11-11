f44485d41d310981bd997e7fd3e60b33783f62e7
Vishal Agrawal <vishalagrawal14@gmail.com>
2013-11-11 13:59:42 -0500

    autoload utility helper + models to connec to database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url','utility');
 
 
 /*
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('Connect_to_mysql');
+$autoload['model'] = array('Connect_to_mysql','Connect_to_tmw');
 
 
 /* End of file autoload.php */
